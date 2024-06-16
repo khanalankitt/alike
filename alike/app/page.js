ccfbe84ea8ffd01b48785a3a2f915b418a774f4b
Ankit Khanal <akhanal749@gmail.com>
2024-06-16 14:04:32 +0545

    add

--- alike/app/page.js
+++ alike/app/page.js
@@ -1,32 +1,9 @@
-import Image from "next/image";
+import Hero from "./hero";
 
 export default function Home() {
   return (
     <>
-      <div className="container">
-        <h2>Love Calculator</h2>
-        <div className="part">
-          <div className="left">
-              <p>Date of birth</p>
-              <input type="date" name="yourdate" required/>
-              <Image
-                src="/man.png"
-                height={100}
-                width={100}
-              />
-          </div>
-          <div className="right">
-            <p>Date of birth</p>
-            <input type="date" name="theirdate" required/>
-            <Image
-                src="/woman.png"
-                height={100}
-                width={100}
-              />
-          </div>
-        </div>
-        <button>Calculate</button>
-      </div>
+      <Hero/>
     </>
   );
 }
